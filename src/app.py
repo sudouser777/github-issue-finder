1a1882dac5513f1997aefd57e4ec02837ee8ec73
Raju Komati <komatiraju032@gamil.com>
2025-03-30 09:18:15 +0530

    Updated script to generate html page.

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 import argparse
 import os
 import sys
-from argparse import Namespace
 
+from bs4 import BeautifulSoup
 from github import Github
 from github.Auth import Token
+from github.Issue import Issue
 
 auth = Token(os.getenv('GITHUB_TOKEN'))
 
@@ -13,9 +14,10 @@ parser.add_argument('--label', help='label to find')
 parser.add_argument('--language', help='language to find')
 parser.add_argument('--limit', default=500, type=int, required=False, help='Minimum number of issues to search')
 parser.add_argument('-s', '--stars', default=10, type=int, required=False, help='minimum number of stars')
+args = parser.parse_args(sys.argv[1:])
 
 
-def find_issues(args: Namespace) -> list:
+def find_issues() -> list[Issue]:
     result = []
     count = 0
     try:
@@ -25,7 +27,7 @@ def find_issues(args: Namespace) -> list:
         while True:
             for issue in issues:
                 count += 1
-                if issue.repository.stargazers_count < args.stars:
+                if issue.repository.stargazers_count >= args.stars:
                     result.append(issue)
 
                 if count == args.limit:
@@ -34,10 +36,46 @@ def find_issues(args: Namespace) -> list:
         print('Error occurred', e)
     return result
 
+def create_table_rows(soup: BeautifulSoup, issues: list[Issue]):
+    table_body = soup.find(id='issues-table').find('tbody')
+    table_body.clear()
+
+    for issue in issues:
+        row = soup.new_tag('tr')
+
+        # Create repository link
+        repo_cell = soup.new_tag('td')
+        repo_link = soup.new_tag('a', href=issue.repository.html_url, target="_blank")
+        repo_link.string = issue.repository.full_name
+        repo_cell.append(repo_link)
+
+        # Create issue link
+        issue_cell = soup.new_tag('td')
+        issue_link = soup.new_tag('a', href=issue.html_url, target="_blank")
+        issue_link.string = f'Issues#{issue.number}'
+        issue_cell.append(issue_link)
+
+        # Create stars count
+        stars_cell = soup.new_tag('td')
+        stars_cell.string = str(issue.repository.stargazers_count)
+
+        # Append the cells to the row
+        row.append(repo_cell)
+        row.append(issue_cell)
+        row.append(stars_cell)
+
+        table_body.append(row)
+
+def find_issues_and_populate_html() -> None:
+    issues = find_issues()
+    with open('template.html') as fp, open('../index.html', 'w') as fw:
+        soup = BeautifulSoup(fp, features='html.parser')
+        create_table_rows(soup, issues)
+        fw.write(soup.prettify())
+
 
 def main():
-    iss = find_issues(parser.parse_args(sys.argv[1:]))
-    print(iss)
+    find_issues_and_populate_html()
 
 
 if __name__ == '__main__':
